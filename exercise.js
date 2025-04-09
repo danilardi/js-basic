2b62e9c6d3ef4bb617e93b3bccbde75dd89eb440
Danil Ardi <danilardi13@gmail.com>
2025-04-09 16:59:08 +0700

    fix: check if vehicle type not found

--- exercise.js
+++ exercise.js
@@ -1,19 +1,27 @@
-function calculateTotalParkingFee(vehicleType, duration) {
-    const rates = {
-        car: {
-            base: 5000,
-            perHour: 3000,
-            extraChargeOver24: 50000
-        },
-        bike: {
-            base: 2000,
-            perHour: 1000,
-            extraChargeOver24: 20000
-        }
+const rates = {
+    car: {
+        feeBase: 5000,
+        feePerHour: 3000,
+        extraChargeOver24: 50000
+    },
+    bike: {
+        feeBase: 2000,
+        feePerHour: 1000,
+        extraChargeOver24: 20000
     }
+}
 
-    let totalFee = rates[vehicleType].base + (rates[vehicleType].perHour * (duration -1));
-    if (duration > 24) {
+const oneDay = 24;
+
+function calculateTotalParkingFee(vehicleType, duration) {
+    if (!rates[vehicleType]) {
+        return
+    }
+    
+    if (duration < 1) duration = 1;
+
+    let totalFee = rates[vehicleType].feeBase + (rates[vehicleType].feePerHour * (duration - 1));
+    if (duration > oneDay) {
         totalFee += rates[vehicleType].extraChargeOver24;
     }
 
@@ -23,3 +31,4 @@ function calculateTotalParkingFee(vehicleType, duration) {
 calculateTotalParkingFee("car", 3);
 calculateTotalParkingFee("bike", 1);
 calculateTotalParkingFee("car", 27);
+calculateTotalParkingFee("bikee", 30);
